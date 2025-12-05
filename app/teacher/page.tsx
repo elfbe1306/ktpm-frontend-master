90eda4a95009b8b93a89d903789d7eb930159e67
SongKhuee <148840532+SongKhuee@users.noreply.github.com>
2025-12-05 23:10:34 +0700

    change in teacher page

--- app/teacher/page.tsx
+++ app/teacher/page.tsx
@@ -1,21 +1,44 @@
 
 import {BarChart} from "@/components/BarChart"
 import { MdTask } from "react-icons/md";
+import { BsFillAlarmFill } from "react-icons/bs";
+import { BsFillFileEarmarkArrowUpFill } from "react-icons/bs";
+import { BiSolidFilePdf } from "react-icons/bi";
+import { FaBookReader } from "react-icons/fa";
+import { BsPeopleFill } from "react-icons/bs";
 export default function TeacherHomePage() {
     
     return (
     <div className="bg-gray-100 p-8 min-h-screen font-sans">
       {/* Main Dashboard Container */}
         <div className="max-w-7xl mx-auto bg-white shadow-xl rounded-lg p-6 space-y-8">
-        <h1 className="text-2xl font-semibold text-gray-800 font-display">Bảng điều khiển</h1>
+          <div className="flex flex-col md:flex-row justify-between items-center gap-4">
+          <h1 className="text-2xl font-semibold text-gray-800 font-display">Bảng điều khiển</h1>
+            
+            {/* Khu vực chứa 2 Dropdown */}
+            <div className="flex flex-row gap-3 w-full md:w-auto">
+                {/* Dropdown Học kỳ */}
+              <select className="bg-white  text-gray-700 text-sm font-display rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full md:w-auto p-2.5 outline-none cursor-pointer">
+                <option value="HK251">HK251</option>
+                <option value="HK243">HK243</option>
+                <option value="HK242">HK242</option>
+                <option value="HK241">HK241</option>
+              </select>
+
+                {/* Dropdown Môn học */}
+              <select className="bg-white border border-gray-300 text-gray-700 text-sm font-display rounded-lg focus:ring-blue-500 block w-full md:w-auto p-2.5 outline-none cursor-pointer">
+                <option value="all">Tất cả môn học</option>
+                <option value="SE104">Kiến trúc phần mềm</option>
+                <option value="IT001">Nhập môn lập trình</option>
+                <option value="IT002">Lập trình hướng đối tượng</option>
+                </select>
+            </div>
+        </div>
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
             <div className="lg:col-span-3 xl:col-span-4 flex flex-row justify-between gap-6">
                 <div className="bg-white p-4 w-150 rounded-lg shadow-md flex flex-col items-start space-y-2 border border-gray-100">
                 <div className="p-3 rounded-xl bg-purple-200 text-purple-700">
-                    <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
-                    </svg>
+                  <FaBookReader size={25}/>
                 </div>
                 <p className="text-sm text-gray-500 font-display">Tổng số khóa học</p>
                 <p className="text-xl font-bold text-gray-800">6</p>
@@ -24,9 +47,7 @@ export default function TeacherHomePage() {
                 {/* Stat Card 2:*/}
                 <div className="bg-white p-4 w-150 rounded-lg shadow-md flex flex-col items-start space-y-2 border border-gray-100">
                 <div className="p-3 rounded-xl bg-cyan-200 text-cyan-700">
-                    <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
-                    </svg>
+                  <BsPeopleFill size={25}/>
                 </div>
                 <p className="text-sm text-gray-500 font-display">Tổng số học sinh</p>
                 <p className="text-xl font-bold text-gray-800">183.000</p>
@@ -56,7 +77,7 @@ export default function TeacherHomePage() {
           <div className="xl:col-span-1 bg-white p-6 rounded-lg shadow-md border border-gray-100 space-y-4">
             <h2 className="text-xl font-semibold text-gray-800">Hoạt động gần đây</h2>
 
-            {/* Message 1 */}
+            {/* Act 1 */}
             <div className="flex items-center space-x-3 hover:bg-gray-50 p-2 -mx-2 rounded-lg cursor-pointer transition-colors">
                 <div className="p-3 rounded-full bg-green-01">
                   <MdTask size={24} color="white" />
@@ -67,38 +88,55 @@ export default function TeacherHomePage() {
               </div>
             </div>
 
-            {/* Message 2 */}
+            {/* Act 2 */}
             <div className="flex items-center space-x-3 hover:bg-gray-50 p-2 -mx-2 rounded-lg cursor-pointer transition-colors">
-              <img 
-                className="w-10 h-10 rounded-full object-cover" 
-                src="https://i.pravatar.cc/150?img=25" 
-                alt="Dean Winchester" 
-              />
-              <div>
-                <p className="font-medium text-gray-800">Dean Winchester</p>
-                <p className="text-sm text-gray-500">@imdean</p>
+                <div className="p-3 rounded-full bg-orange-01">
+                  <BsFillAlarmFill size={24} color="white" />
+                </div>
+              <div className="flex-1">
+                <p className="font-medium text-gray-800">Bài kiểm tra số 1</p>
+                <p className="text-sm text-gray-500">Kiến trúc phần mềm</p>
               </div>
             </div>
 
-            {/* Message 3 */}
+            {/* Act 3 */}
             <div className="flex items-center space-x-3 hover:bg-gray-50 p-2 -mx-2 rounded-lg cursor-pointer transition-colors">
-              <img 
-                className="w-10 h-10 rounded-full object-cover" 
-                src="https://i.pravatar.cc/150?img=42" 
-                alt="John Dodol" 
-              />
-              <div>
-                <p className="font-medium text-gray-800">John Dodol</p>
-                <p className="text-sm text-gray-500">@dodoljohn</p>
+                <div className="p-3 rounded-full bg-red-01">
+                  <BsFillFileEarmarkArrowUpFill size={24} color="white" />
+                </div>
+              <div className="flex-1">
+                <p className="font-medium text-gray-800">Bài tập lớn 1</p>
+                <p className="text-sm text-gray-500">Kiến trúc phần mềm</p>
               </div>
             </div>
 
-            {/* Button */}
-            <div className="pt-2">
-              <button className="w-full py-2 border border-blue-500 text-blue-500 font-semibold rounded-lg hover:bg-blue-50 transition-colors">
-                Start Conversation
-              </button>
+            {/* Act 4 */}
+            <div className="flex items-center space-x-3 hover:bg-gray-50 p-2 -mx-2 rounded-lg cursor-pointer transition-colors">
+              <BiSolidFilePdf size={50} color="F1351B"/>
+              <div className="flex-1">
+                <p className="font-medium text-gray-800">Chương 3</p>
+                <p className="text-sm text-gray-500">Kiến trúc phần mềm</p>
+              </div>
             </div>
+
+            {/* Act 5 */}
+            <div className="flex items-center space-x-3 hover:bg-gray-50 p-2 -mx-2 rounded-lg cursor-pointer transition-colors">
+              <BiSolidFilePdf size={50} color="F1351B"/>
+              <div className="flex-1">
+                <p className="font-medium text-gray-800">Chương 2</p>
+                <p className="text-sm text-gray-500">Kiến trúc phần mềm</p>
+              </div>
+            </div>
+
+            {/* Act 6 */}
+            <div className="flex items-center space-x-3 hover:bg-gray-50 p-2 -mx-2 rounded-lg cursor-pointer transition-colors">
+              <BiSolidFilePdf size={50} color="F1351B"/>
+              <div className="flex-1">
+                <p className="font-medium text-gray-800">Chương 1</p>
+                <p className="text-sm text-gray-500">Kiến trúc phần mềm</p>
+              </div>
+            </div>
+
           </div>
         </div>
       </div>
