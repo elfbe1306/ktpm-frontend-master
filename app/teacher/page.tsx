f4dd22f97284088d7c2ad01ee29be7f6b70553c9
SongKhuee <148840532+SongKhuee@users.noreply.github.com>
2025-12-05 15:17:38 +0700

    update color and create recent activities

--- app/teacher/page.tsx
+++ app/teacher/page.tsx
@@ -1,5 +1,6 @@
 
 import {BarChart} from "@/components/BarChart"
+import { MdOutlineTask } from "react-icons/md";
 export default function TeacherHomePage() {
     
     return (
@@ -35,10 +36,10 @@ export default function TeacherHomePage() {
 
         <hr className="border-gray-200" />
 
-        {/* Main Content Area (Graph & Messages) */}
+        {/* Main Content Area (Graph & Activities) */}
         <div className="grid grid-cols-1 xl:grid-cols-3 gap-6">
           
-          {/* Profile Visit Chart (Left Column) */}
+          {/* Bar Chart (Left Column) */}
           <div className="xl:col-span-2 bg-white p-6 rounded-lg shadow-md border border-gray-100">
             <div className="flex justify-between items-center mb-4">
               <h2 className="text-xl font-semibold text-gray-800">Số lượng học sinh có điểm trung bình giỏi và yếu theo từng khóa học</h2>
@@ -51,17 +52,15 @@ export default function TeacherHomePage() {
             <BarChart/>
           </div>
 
-          {/* Recent Messages (Right Column) */}
+          {/* Recent Activities (Right Column) */}
           <div className="xl:col-span-1 bg-white p-6 rounded-lg shadow-md border border-gray-100 space-y-4">
-            <h2 className="text-xl font-semibold text-gray-800">Recent Messages</h2>
+            <h2 className="text-xl font-semibold text-gray-800">Hoạt động gần đây</h2>
 
             {/* Message 1 */}
             <div className="flex items-center space-x-3 hover:bg-gray-50 p-2 -mx-2 rounded-lg cursor-pointer transition-colors">
-              <img 
-                className="w-10 h-10 rounded-full object-cover" 
-                src="https://i.pravatar.cc/150?img=11" 
-                alt="Hank Schrader" 
-              />
+                <div className="p-3 rounded-xl bg-cyan-200 text-cyan-700">
+                  <MdOutlineTask size={24} />
+                </div>
               <div className="flex-grow">
                 <p className="font-medium text-gray-800">Hank Schrader</p>
                 <p className="text-sm text-gray-500">@johnducky</p>
